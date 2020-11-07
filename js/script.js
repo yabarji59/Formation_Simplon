2a9ac1b492e0efa8dac4b455b8e0ae3dee71053f
JustineHga <lemairejustine01@gmail.com>
2020-11-07 14:26:32 +0100

    plan JavaScript AfficherCacher

--- js/script.js
+++ js/script.js
@@ -61,3 +61,17 @@ $(document).ready(function(){
         }
 /** / event.changeImage */
 
+function afficher_cacher(id)
+{
+    if(document.getElementById(id).style.visibility=="hidden")
+    {
+        document.getElementById(id).style.visibility="visible";
+        document.getElementById('bouton_'+id).innerHTML='RÉDUIRE';
+    }
+    else
+    {
+        document.getElementById(id).style.visibility="hidden";
+        document.getElementById('bouton_'+id).innerHTML='DÉCOUVRIR';
+    }
+    return true;
+}
--- modules/plan/views/planView.php
+++ modules/plan/views/planView.php
@@ -5,162 +5,84 @@
       <h1>SENS DE<span id="B">L'ORIENTATION </span><img src="./img/logos/brunembert.png" alt="logo brunembert"></h1>
   </div>
 
-    <section class="nav-select">
-      <nav class="nav-select">
-        <ul>
-          <a href="#accueil"><h2>1. Accueil</h2></a>
-          <a href="#documents"><h2>2. Documents</h2></a>
-          <a href="#equipe"><h2>3. L'équipe communale</h2></a>
-          <a href="#articlest"><h2>5. Articles</h2></a>
-          <a href="#galerie"><h2>4. Galerie photo</h2></a>
-          <a href="#contact"><h2>5. Contact</h2></a>
-          <a href="#illectronisme"><h2>7. Illectronisme</h2></a>
-          <a href="#presse"><h2>8. Presse</h2></a>
-          <a href="#mentions"><h2>9. Mentions Légales</h2></a>
-          <a href="#faq"><h2>10. Foire aux questions</h2></a>
-          <a href="#admin"><h2>11. Administration | ( privé )</h2></a>
-          <a href="#plan"><h2>12. Plan du site </h2></a>
-        </ul>
-        </nav><!-- fin nav plan -->
-    </section>
+  <span class="bouton" id="bouton_texte" onclick="javascript:afficher_cacher('texte');">découvrir les pages</span>
+    <div id="texte" class="texte">
+            <a href="index.php"><h2>1. Accueil</h2>
+             <p>
+                      Sur cette page, vous y trouverez les actualités, des sondages, un raccourci vers la page Facebook ainsi qu'un bref aperçu du village et 
+                      de son histoire
+            </p>
+            </a>
 
-    <section class="screens">
-      <a href="index.php"><h2>Accueil</h2><span class="grey-right-part">Espace d'accueil lorsque vous entrez sur le site internet de Brunembert</span></a>
-        <div class="screenshots-container-display" id="accueil">
-            <h2>Aperçu de la page<em class="green-word"> d'Accueil</em></h2>
-            <img src="/1.png" alt="screenshot accueil" id="accueil" class="screenshots">
-              <div class="texte-affiche-display">
-                  <p>Par le biais de la molette de la souris, vous pouvez descendre sur la page d'Accueil pour avoir accès à toutes les informations qui s'y trouvent
-                <br><br>
-              Sur cette page, vous y trouverez les actualités, des sondages, un raccourci vers le groupe facebook d'espaces et d'échanges du village ainsi qu'un bref aperçu du village et 
-              de son histoire</p>
-            </div>
-        </div>
+          <a href="index.php?page=documents"><h2>2. Documents</h2>
         
-      <a href="index.php?page=documents"><h2>Documents</h2><span class="grey-right-part">Espace d'archives, documents liés à Brunembert, à la nation, aux habitants. Ils sont libres d'accès, à la consultation et au téléchargement</span></a>
-        <div class="screenshots-container-display">
-          <h2>Aperçu de la page <em class="green-word"> Mes Documents</em></h2>
-            <img src="/4.png" alt="screenshot documents" id="documents" class="screenshots">
-          <div class="texte-affiche-display">
-              <p>Par le biais de la molette de la souris, vous pouvez descendre sur la page documentspour avoir accès à toutes les informations qui s'y trouvent
-              <br><br>
-              Sur cette page, vous y trouverez des documents photos et vidéos liés à la santé, à l'Histoire, au conseil, à l'économie du village...Que vous pourrez télécharger </p>
-          </div>
-        </div>
-
-      <a href="index.php?page=equipe"><h2>L'équipe communale</h2><span class="grey-right-part">La page qui recense les membres du Conseil Municipal. Vous pouvez y trouver un lien de référence concernant les élections</span></a>
-        <div class="screenshots-container-display">
-          <h2>Aperçu de la page<em class="green-word"> Équipe communale</em></h2>
-            <img src="" alt="screenshot equipe" id="equipe" class="screenshots">
-            <div class="texte-affiche-display">
-              <p>Par le biais de la molette de la souris, vous pouvez descendre sur la page Équipe communale pour avoir accès à toutes les informations qui s'y trouvent
-              <br><br>
+                  <p>
+                  Sur cette page, vous y trouverez des documents photos et vidéos liés à la santé, à l'Histoire, au conseil, à l'économie du village disponibles au téléchargement
+                  </p>
+          
+            </a>
+          <a href="index.php?page=equipe"><h2>3. L'équipe communale</h2>
+      
+              <p>
               Sur cette page, vous y trouverez les membres constitutionnels du conseil municipal en date ainsi qu'une intégration web " carte des conseils municipaux " du Pas-de-Calais</p>
-          </div>
-        </div>
-
-        <a href="index.php?page=articles"><h2>Les articles</h2><span class="grey-right-part">La page qui recense les membres du Conseil Municipal. Vous pouvez y trouver un lien de référence concernant les élections</span></a>
-        <div class="screenshots-container-display">
-          <h2>Aperçu de la page<em class="green-word"> Articles</em></h2>
-            <img src="" alt="screenshot equipe" id="equipe" class="screenshots">
-            <div class="texte-affiche-display">
-              <p>Connaître les dernières infos et événements! 
-              <br><br>
-              Sur cette page, vous y trouverez les détails des dernières actualités </p>
-          </div>
-        </div>
-
-        <a href="index.php?page=galerie"><h2>Galerie photo</h2><span class="grey-right-part">La page où vous pourrez retrouver les photos relatives au Village de Brunembert</span></a>
-        <div class="screenshots-container-display">
-            <h2>Aperçu de la page<em class="green-word"> Galerie photo</em></h2>
-            <img src="/7.png" alt="screenshot galerie"  id="galerie"  class="screenshots">
-            <div class="texte-affiche-display">
-                <p>Par le biais de la molette de la souris, vous pouvez descendre sur la page Galerie photos pour avoir accès à toutes les informations qui s'y trouvent
-              <br><br>
-              Sur cette page, vous y trouverez des photos des évènements, actualités et dynamiques du village à consulter avec ou sans musique.</p>
-          </div>
-        </div>
-
-      <a href="index.php?page=contact"><h2>Contact</h2><span class="grey-right-part">Besoin d'aide? Vous pouvez être accompagnés dans vos démarches par le biais des informations que vous retrouverez sur cette page</span></a>
-        <div class="screenshots-container-display">
-          <h2>Aperçu de la page<em class="green-word"> Contact</em></h2>
-            <img src="/3.png" alt="screenshot contact" id="contact" class="screenshots">
-            <div class="texte-affiche-display">
-              <p>Par le biais de la molette de la souris, vous pouvez descendre sur la page Contact pour avoir accès à toutes les informations qui s'y trouvent
-              <br><br>
-              Sur cette page, vous y trouverez le mail et le numéro de contact du secrétariat de la mairie, les horaires de permanences, la météo, un lien vers la page Facebook du village.</p>
-          </div>
-        </div>
-
-      <a href="index.php?page=illectronisme"><h2>Illectronisme</h2><span class="grey-right-part">Parce que tout le monde n'a pas la culture numérique, il est important d'offrir des moyens de palier à ces connaissances devenues indispensables</span></a>    
-        <div class="screenshots-container-display">
-          <h2>Aperçu de la page<em class="green-word"> Illectronisme</em></h2>
-            <img src="/8.png" alt="screenshot illectronisme" id="illectrionisme" class="screenshots">
-            <div class="texte-affiche-display">
-              <p>Par le biais de la molette de la souris, vous pouvez descendre sur a page Illectronisme pour avoir accès à toutes les informations qui s'y trouvent
-              <br><br>
-              Sur cette page, vous y trouverez des informations concernant le constat de notre société relatif à la fracture numérique, des outils pour y remédier ainsi que des contacts</p>
-          </div>
-        </div>
-
-      <a href="index.php?page=presse"><h2>Presse</h2><span class="grey-right-part">Retrouvez la source des articles de presse publiés sur internet qui mettent en avant notre beau Village </span></a>
-        <div class="screenshots-container-display">
-            <h2>Aperçu de la page<em class="green-word"> Presse</em></h2>
-            <img src="/10.png" alt="screenshot presse"  id="presse" class="screenshots">
-            <div class="texte-affiche-display">
-                <p>Par le biais de la molette de la souris, vous pouvez descendre sur la page Presse pour avoir accès à toutes les informations qui s'y trouvent
-                <br><br>
-                Sur cette page, vous y trouverez deux liens de redirection vers les sites de Presse locale, La semaine dans le Boulonnais et La Voix du Nord</p>
-            </div>
-        </div>
-
-      <a href="index.php?page=mentions"><h2>Mentions Légales</h2><span class="grey-right-part">Vous y retrouverez toutes les informations relatives au développement du site internet de Brunembert</span></a>
-        <div class="screenshots-container-display">
-            <h2>Aperçu de la page<em class="green-word"> Mentions Légales</em></h2>
-              <img src="/9.png" alt="screenshot mentions" id="mentions"  class="screenshots">
-            <div class="texte-affiche-display">
-              <p>Par le biais de la molette de la souris, vous pouvez descendre sur la page mentions pour avoir accès à toutes les informations qui s'y trouvent
-              <br><br>
-              Sur cette page, vous y trouverez les informations légales concernant le développement du site internet de la Mairie de Brunembert.</p>
-          </div>
-        </div>
-
-      <a href="index.php?page=faq"><h2>Foire aux questions</h2><span class="grey-right-part">Un espace de premières réponses à vos questions, en cas d'insatisfaction, merci de vous diriger vers le secrétariat de la commune </span></a>
-        <div class="screenshots-container-display">
-          <h2>Aperçu de la page<em class="green-word"> Foire aux questions</em></h2>
-          <img src="/6.png" alt="screenshot faq" id="faq" class="screenshots">
-            <div class="texte-affiche-display">
-              <p>Par le biais de la molette de la souris, vous pouvez descendre sur la page Foire aux Questions pour avoir accès à toutes les informations qui s'y trouvent
-                <br><br>
-                Sur cette page, vous y trouverez les questions fréquentes posées au secrétariat de la mairie de Brunembert.</p>
-              </div>
-        </div>
-
-      <a href="index.php?page=admin"><h2>Administration | ( privé )</h2><span class="grey-right-part">Espace de gestion du site dédié au Conseil Municipal</span></a>
-        <div class="screenshots-container-display">
-          <h2>Aperçu de la page<em class="green-word"> Administration</em></h2>
-            <img src="/11.png" alt="screenshot admin" id="admin" class="screenshots">
-              <div class="texte-affiche-display">
-              <p>Par le biais de la molette de la souris, vous pouvez descendre sur la page Admin pour avoir accès à toutes les informations qui s'y trouvent
-                <br><br>
-                Sur cette page, vous y trouverez un module de connexion réservée aux membres Administratifs de maintenance du site internet.</p>
-            </div>
-        </div>
-
-      <a href="index.php?page=plan"><h2>Plan</h2><span class="grey-right-part">Espace d'orientation sur le site</span></a>
-        <div class="screenshots-container-display">
-          <h2>Aperçu de la page<em class="green-word"> Plan</em></h2>
-            <img src="/12.png" alt="screenshot plan" id="plan" class="screenshots">
-              <div class="texte-affiche-display">
-              <p>Par le biais de la molette de la souris, vous pouvez descendre sur la page plan pour avoir accès à toutes les informations qui s'y trouvent
-                <br><br>
-                Sur cette page, vous y trouverez des repères pour naviguer sur le site de Brunembert, toutes les pages y sont recensées.
-              Dernière mise à jour: 17/06/2020 22H29</p>
-            </div>
-        </div>
+              </p>
         
-    </section>
+          </a>
+          <a href="index.php?page=articles"><h2>5. Articles</h2>
+      
+              <p>
+              Sur cette page, vous y trouverez des renseignements supplémentaires d'ordre culturel, sanitaire, social, politique... 
+              </p>
+        
+          </a>
+          <a href="index.php?page=galerie"><h2>4. Galerie photo</h2>
+      
+              <p>
+              Sur cette page, vous y trouverez des photos des évènements, actualités et dynamiques du village à consulter ou à enregistrer</p>
+              </p>
+        
+          </a>
+          <a href="index.php?page=contact"><h2>5. Contact</h2>
+      
+              <p>
+              Sur cette page, vous pourrez joindre directement le secrétariat de la mairie de Brunembert via un formulaire mais aussi trouver le mail direct, le numéro de contact
+              et l'adresse.</p>
+              </p>
+        
+          </a>
+          <a href="index.php?page=illectronisme"><h2>7. Illectronisme</h2>
+      
+              <p>Sur cette page, vous y trouverez des informations concernant l'inclusion numérique soit, des solutions pour équiper à la maison, vous former et avoir un diagnostic de vos
+                compétences numériques
+              </p>
+          
+        </a>
+          <a href="index.php?page=presse"><h2>8. Presse</h2>
+    
+              <p> Sur cette page, vous y trouverez deux liens de redirection vers les sites de deux Presses locales, La semaine dans le Boulonnais et La Voix du Nord</p>
+         
+          </a>
 
+          <a href="index.php?page=mentions"><h2>9. Mentions Légales</h2></a>
+      
+              <p>Sur cette page, vous y trouverez les informations légales concernant le développement du site internet de la Mairie de Brunembert.</p>
+         
+          </a>
 
+          <a href="index.php?page=faq"><h2>10. Foire aux questions</h2>
+      
+              <p> Sur cette page, vous y trouverez les questions fréquentes posées au secrétariat de la mairie de Brunembert.</p>
+        
+          </a>
+
+          <a href="index.php?page=admin"><h2>11. Administration | ( privé )</h2>
+          <p>privé - maintenance du site </p>
+          </a>
+
+  </div><!-- class texte -->
 </div><!-- fin div class content-plan -->
-
+    <script type="text/javascript">
+    //<!--
+        afficher_cacher('texte');
+    //-->
+    </script>

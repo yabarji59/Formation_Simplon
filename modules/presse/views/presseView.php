e1971e74c5b6b6a1bf29f2be72fa8442abc23661
JustineHga <lemairejustine01@gmail.com>
2020-11-15 18:59:56 +0100

    presse modifiée

--- modules/presse/views/presseView.php
+++ modules/presse/views/presseView.php
@@ -7,13 +7,13 @@
 
   <div class="presse-btn">
 
-    <a href="https://www.lavoixdunord.fr/region/boulogne-et-ses-environs/brunembert" target="_blanck" 
+    <a href="https://www.lavoixdunord.fr/region/boulogne-et-ses-environs/brunembert" target="_blank" 
     class="btn btn-lg btn-primary" id="vdn">
     <span class="glyphicon glyphicon-list-alt"></span> Brunembert dans la Voix du Nord  <br>
     <img class=""  alt="logo voix du nord" src="./img/logos/vdn.jpg"></a>
 
     <a href="https://www.lasemainedansleboulonnais.fr/archives/recherche?word=brunembert&sort=weight&datefilter=lastyear&form_build_id=form-G5YmAWizzq2GsQ3umPw386UyZ7nx5pcJdNVpPfS96Dw&form_id=dpidamwidgets_damsimplesearch_content_type_search_form" 
-    target="_blanck" class="btn btn-lg btn-primary" id="sdb"><span class="glyphicon glyphicon-list-alt"></span> La Semaine dans le Boulonnais                       
+    target="_blank" class="btn btn-lg btn-primary" id="sdb"><span class="glyphicon glyphicon-list-alt"></span> La Semaine dans le Boulonnais                       
     <br> 
     <img class="" alt="logo voix du nord" src="./img/logos/sdb.jpg"></a>
 

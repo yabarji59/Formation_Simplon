ccc6631da0ab5c5d7d6c5baa13f267951c34ac75
Justine Lemaire <lemairejustine01@gmail.com>
2021-09-15 14:35:21 +0200

    Ajout Java

---
+++ src/fr/eCommerce/test/Calculs.java
@@ -0,0 +1,19 @@
+package fr.eCommerce.test;
+
+public class Calculs {
+    public static void main(String[] args){
+        // fonction 1: soustraction 
+        int a = 1992;
+        int b = 2021;
+        String nameJ = "Justine";
+        System.out.println( nameJ + " a " + ( b-a ) + " ans");
+
+        // fonction 2:  phrase 
+
+        String userName = "Maria";
+        String place = "Lille"; // String = type wrapper/ Objet 
+
+        int codePostal = 59000;
+        System.out.println(" et" + userName + " " + "habite au " + codePostal + " " + place );
+    }
+}
